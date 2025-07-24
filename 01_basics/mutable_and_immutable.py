6b0e5cc40b833a5d75ffab5d54a30dc23372cd2d
Chashmishlish <salishkhannn@gmail.com>
2025-07-24 17:33:20 +0500

    Understanding of Mutable & Immutable!

---
+++ 01_basics/mutable_and_immutable.py
@@ -0,0 +1,34 @@
+# Mutable   : which can be changed
+# Immutable : which cant be changed
+
+# Object bola gaya hai python me sab data types ko - int, str
+# mostly datatypes in python are immutable
+
+
+username = "Alishba"
+username
+
+username = "khan"
+username 
+
+x = 10
+y = x   # x me jo bhi value the y me daldo
+print(x)
+print(y)
+
+x = 15 
+print(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
