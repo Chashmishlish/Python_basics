ed0061580b9e4382dfea12294ab4d3ca1ffbab81
Chashmishlish <salishkhannn@gmail.com>
2025-07-24 18:09:46 +0500

    basic overview!

--- 01_basics/Overview_datatypes.py
+++ 01_basics/Overview_datatypes.py
@@ -1,5 +1,25 @@
 # big picture of data types
-# basic overview
+# basic high-end overview
 
 mylist = [1, 2, 3, ['a', 'b']]
 mylist
+
+import random
+random.random()
+random.choice([1,2,3,4,5,6,7,8])
+
+import math
+math.pi
+
+2 ** 100
+
+username = "Alishba khan"
+len(username)
+
+username [0]  #somehow string is also treated as list internally
+
+username[0] = 'A' #error: TypeError: 'str' object does not support item assignment
+
+username [-1] #n
+username [1:5] #lish
+
